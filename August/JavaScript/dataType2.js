86d259532dee4c89aa53e1ceea1f334822618e2f
sangminpark <qpwpep5429@gmail.com>
2020-08-05 13:12:13 +0900

    오타수정

---
+++ August/JavaScript/dataType2.js
@@ -0,0 +1,18 @@
+// 숫자형과 문자열 재료
+var material1 = '3';
+var material2 = 3;
+var material3 = '4';
+var material4 = 10;
+
+// 요리 결과
+var result1 = String(material2 * material4);
+var result2 = Number(material3) * material4;
+
+// 형 변환을 사용해서 result1에는 문자열 '30'을, result2에는 숫자형 40을 만들어 넣어보세요.
+// 코드를 작성하세요.
+
+// 테스트 코드
+console.log(result1);
+console.log(typeof result1);
+console.log(result2);
+console.log(typeof result2);
