ba9ba4bf24524db2b9a4cfef210b2f1dfc7fad61
sangminpark <qpwpep5429@gmail.com>
2020-08-05 12:53:40 +0900

    JS 8/5

---
+++ August/JavaScript/dataType1.js
@@ -0,0 +1,21 @@
+// 숫자형과 문자열 재료
+var material1 = 3;
+var material2 = '3';
+var material3 = 10;
+var material4 = '4';
+var material5 = 4;
+
+// 연산 결과
+var result1;
+result1 = '34';
+var result2;
+result2 = 34;
+
+// 연산을 통해 result1에 문자열 '34'를, result2에 숫자형 34를 만들어 넣어주세요.
+// 코드를 작성하세요.
+
+// 테스트 코드
+console.log(result1);
+console.log(typeof result1);
+console.log(result2);
+console.log(typeof result2);
---
+++ August/JavaScript/numeric.js
@@ -0,0 +1,14 @@
+var width = 10;
+var height = 20;
+
+// 둘레의 길이 구하기
+var perimeter = (width+height) * 2;
+
+
+// 넓이 구하기
+var area = width * height;
+// 코드를 작성하세요.
+
+// 테스트 코드
+console.log("둘레의 길이: " + perimeter);
+console.log("넓이: " + area);
